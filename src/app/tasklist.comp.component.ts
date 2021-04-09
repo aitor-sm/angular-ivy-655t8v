3d4e6aaa9c55488c991f4c96c845042d78762bfc
aitor-sm <aitor.sm@gmail.com>
2021-04-10 01:11:16 +0200

    2 files modified

--- src/app/tasklist.comp.component.ts
+++ src/app/tasklist.comp.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+//import {MatDialogModule} from '@angular/material/dialog';
 import { TaskObj, FlowActionObj, basicFlow } from "./tasks";
 import { TASKS } from "./mock-tasks";
 
